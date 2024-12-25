19f0a400be27bfc649ce5a00c6ed7188b9b50c39
Filipe Cardoso <filipescrdoso@gmail.com>
2024-12-25 14:42:12 -0300

    Style updated
    - First version of style created;

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,4 @@
-const menuButton = document.getElementById("menu-button");
-const menu = document.getElementById("menu");
-const navBar = document.getElementById("nav-bar");
-
-menuButton.addEventListener('click', () => {
-    menu.style.display === "none" ? menu.style.display = "flex" : menu.style.display = "none";
-    menu.style.display === "none" ? navBar.style.display = "flex": navBar.style.display = "block";
-})
+function toggleElements(elementName, className) {
+    let element = document.getElementById(elementName);
+    element.classList.toggle(className);
+}
