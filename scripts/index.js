d53f2c184e028ebff8bd73941f21e28d45711d2c
Filipe Cardoso <filipescrdoso@gmail.com>
2024-12-26 17:28:51 -0300

    More changes in style
    - A lot of additions in style;
    - Some adjusts and corrections;

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-function toggleElements(elementName, className) {
-    let element = document.getElementById(elementName);
+function toggleElements(elementId, className) {
+    let element = document.getElementById(elementId);
     element.classList.toggle(className);
 }
